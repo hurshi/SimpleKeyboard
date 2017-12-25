4200c97e8faa81334146edf9398c7908be0a4927
化十 <e_Gavin@163.com>
2017-12-25 16:23:32 +0800

    fix bug

--- library/src/main/java/hurshi/github/com/library/SimpleKeyboard.java
+++ library/src/main/java/hurshi/github/com/library/SimpleKeyboard.java
@@ -15,7 +15,6 @@ import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import java.util.List;
